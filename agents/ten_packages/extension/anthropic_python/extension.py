71c2925fbcc0bec289eae3cca912c47b4467f4f2
Jay Zhang <wangyoucao577@gmail.com>
2024-10-13 15:07:03 +0000

    feat: basic config

--- agents/ten_packages/extension/anthropic_python/extension.py
+++ agents/ten_packages/extension/anthropic_python/extension.py
@@ -16,30 +16,62 @@ from ten import (
     Data,
 )
 from .log import logger
+from dataclasses import dataclass, fields
+import anthropic
+
+
+@dataclass
+class AnthropicExtensionConfig:
+    api_key: str = ""
+    model: str = "claude-3-5-sonnet-20240620"
+    max_tokens: int = 512
+    prompt: str = ""
+    greeting: str = ""
+
+    def init_vars_from_ten_property(self, ten_env: TenEnv):
+        for field in fields(self):
+            if not ten_env.is_property_exist(field.name):
+                continue
+            match field.type:
+                case 'str':
+                    val = ten_env.get_property_string(field.name)
+                    setattr(self, field.name, val)
+                    logger.info(f"{field.name}={val}")
+                case 'int':
+                    val = ten_env.get_property_int(field.name)
+                    setattr(self, field.name, val)
+                    logger.info(f"{field.name}={val}")
+                case _:
+                    pass
 
 
 class AnthropicExtension(Extension):
-    def on_init(self, ten_env: TenEnv) -> None:
-        logger.info("AnthropicExtension on_init")
-        ten_env.on_init_done()
+    def __init__(self, name: str):
+        super().__init__(name)
+
+        self.config = AnthropicExtensionConfig()
+        self.client = None
 
     def on_start(self, ten_env: TenEnv) -> None:
         logger.info("AnthropicExtension on_start")
 
-        # TODO: read properties, initialize resources
+        # update config from property
+        self.config.init_vars_from_ten_property(ten_env)
+
+        # initialize client
+        self.client = anthropic.Anthropic(
+            api_key=self.config.api_key,
+        )
 
         ten_env.on_start_done()
 
     def on_stop(self, ten_env: TenEnv) -> None:
         logger.info("AnthropicExtension on_stop")
 
-        # TODO: clean up resources
+        # clean up resources
+        self.client = None
 
         ten_env.on_stop_done()
-
-    def on_deinit(self, ten_env: TenEnv) -> None:
-        logger.info("AnthropicExtension on_deinit")
-        ten_env.on_deinit_done()
 
     def on_cmd(self, ten_env: TenEnv, cmd: Cmd) -> None:
         cmd_name = cmd.get_name()
@@ -52,6 +84,15 @@ class AnthropicExtension(Extension):
 
     def on_data(self, ten_env: TenEnv, data: Data) -> None:
         # TODO: process data
+
+        # message = self.client.messages.create(
+        #     model="claude-3-5-sonnet-20240620",
+        #     max_tokens=1024,
+        #     messages=[
+        #         {"role": "user", "content": "Hello, Claude"}
+        #     ]
+        # )
+        # print(message.content)
         pass
 
     def on_audio_frame(self, ten_env: TenEnv, audio_frame: AudioFrame) -> None:
